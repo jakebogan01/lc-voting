1031f1eac6bd810e538ac254ad0e9119473948b2
Jake Bogan <jakebogan01@gmail.com>
2022-01-27 00:45:58 -0500

    adds nav and inputs on home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,14 +16,70 @@
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans text-gray-900 text-sm bg-red-500">
+    <body class="font-sans text-gray-900 text-sm bg-[#f7f8fc]">
         <header class="flex items-center justify-between px-8 py-4">
-            <a href="#">Laracast Logo</a>
-            <div>
+            <a href="#">
+                <img src="{{ asset('img/logo.svg') }}" alt="Logo">
+            </a>
+            <div class="flex items-center">
+                @if (Route::has('login'))
+                    <div class="px-6 py-4 sm:block">
+                        @auth
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <a href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                   this.closest('form').submit();">
+                                    {{ __('Log Out') }}
+                                </a>
+                            </form>
+                        @else
+                            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
                 <a href="#">
                     <img src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp" alt="avatar" class="w-10 h-10 rounded-full">
                 </a>
             </div>
         </header>
+
+        <main class="container mx-auto max-w-[62.5rem] flex">
+            <div class="max-w-[17.5rem] mr-5">
+                add idea form goes here. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias autem, blanditiis, deleniti error facilis harum illo itaque mollitia nisi nobis, quos reiciendis suscipit ut veniam vero! Cum dolores eveniet quibusdam! Amet aspernatur at culpa fuga nobis obcaecati perferendis sapiente unde.
+            </div>
+            <div class="max-w-[43.75rem]">
+                <nav class="flex items-center justify-between text-xs">
+                    <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+                        <li>
+                            <a href="#" class="border-b-4 pb-3 border-[#328af1]">All Ideas (87)</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-[#328af1]">Considering (6)</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-[#328af1]">In Progress (1)</a>
+                        </li>
+                    </ul>
+
+                    <ul class="flex uppercase font-semibold border-b-4 pb-3 ml-20 space-x-10">
+                        <li>
+                            <a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-[#328af1]">Implemented (10)</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-[#328af1]">Closed (55)</a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div class="mt-8">
+                    {{ $slot }}
+                </div>
+            </div>
+        </main>
     </body>
 </html>
